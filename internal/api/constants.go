ddb5ee55c74ed9e777183af67299104c36b27ddb
Ruslan Kolegov <ruslan@fins.dev>
2023-12-11 22:02:58 +0600

    add constant

--- internal/api/constants.go
+++ internal/api/constants.go
@@ -5,4 +5,5 @@ const (
 	incorrectDataFormat = "Incorrect data format, double check request body"
 	noIdProvided        = "No task id provided for update"
 	noTasksFound        = "No tasks found"
+	invalidIdFormat     = "Invalid id format, should be a number"
 )
