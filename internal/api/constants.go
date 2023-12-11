a73f7002f18eb78c51bdb25c8f5e30d605506832
Ruslan Kolegov <ruslan@fins.dev>
2023-12-11 20:29:30 +0600

    add constants

--- internal/api/constants.go
+++ internal/api/constants.go
@@ -1,10 +1,12 @@
 package api
 
 const (
+	// api error messages
 	incorrectDataFormat = "Incorrect data format, double check request body"
-)
+	noIdProvided        = "No task id provided for update"
+	noTasksFound        = "No tasks found"
 
-const (
+	// task statuses
 	todo       = "to do"
 	inProgress = "in progress"
 	done       = "done"
