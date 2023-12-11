124277c92a55342be297476bbc42838f3ebbde6f
Ruslan Kolegov <ruslan@fins.dev>
2023-12-11 20:26:27 +0600

    add task model with validator

--- internal/api/models.go
+++ internal/api/models.go
@@ -2,7 +2,7 @@ package api
 
 import "errors"
 
-type Params struct {
+type Task struct {
 	ID          int64  `json:"id"`
 	Name        string `json:"name"`
 	Assignee    string `json:"assignee"`
@@ -10,17 +10,17 @@ type Params struct {
 	Description string `json:"description"`
 }
 
-func (p *Params) Validate() error {
-	if p.Name == "" {
+func (t *Task) Validate() error {
+	if t.Name == "" {
 		return errors.New("task name can't be empty")
 	}
 
-	if p.Description == "" {
+	if t.Description == "" {
 		return errors.New("task description can't be empty")
 	}
 
-	if p.Status != "" {
-		if p.Status != todo && p.Status != inProgress && p.Status != done {
+	if t.Status != "" {
+		if t.Status != todo && t.Status != inProgress && t.Status != done {
 			return errors.New(`invalid task status, can be either "to do" or "in progress" or "done"`)
 		}
 	}
