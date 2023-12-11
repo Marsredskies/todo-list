5ecba0259fcbd90705a080bc1e6a483c58f7c487
Ruslan Kolegov <ruslan@fins.dev>
2023-12-11 20:30:29 +0600

    add tests for task save and udpate

---
+++ internal/api/handlers_test.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Marsredskies/todo-list/internal/db"
+	"github.com/Marsredskies/todo-list/internal/envconfig"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSaveAndUpdateTask(t *testing.T) {
+	ctx := context.Background()
+
+	cnf := envconfig.MustGetConfig()
+
+	dbConn, err := db.ConnectDB(ctx, cnf)
+	require.NoError(t, err)
+
+	db.DropMigrations(dbConn)
+	db.MustApplyMigrations(ctx, cnf)
+
+	createParams := Task{
+		Name:        "test task",
+		Description: "description for the test task",
+		Assignee:    "smbd",
+		Status:      "",
+	}
+
+	require.NoError(t, createParams.Validate())
+
+	api := MustInitNewAPI(ctx, cnf)
+
+	id, err := api.saveTaskToDb(ctx, createParams)
+	require.NoError(t, err)
+
+	require.Equal(t, int64(1), id)
+
+	updateParams := Task{
+		ID:          id,
+		Name:        "test task update",
+		Description: "updated description for the test task",
+		Assignee:    "smbd else",
+		Status:      "done",
+	}
+
+	err = api.updateTaskById(ctx, updateParams)
+	require.NoError(t, err)
+}
