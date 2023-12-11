02a1638e8803f72af6f1301557ccf8a820d520b1
Ruslan Kolegov <ruslan@fins.dev>
2023-12-11 20:29:16 +0600

    add database methods implementation

---
+++ internal/db/methods.go
@@ -0,0 +1,31 @@
+package db
+
+import "context"
+
+func (d *DB) ExecCtxReturningId(ctx context.Context, query string, args ...interface{}) (int64, error) {
+	var id int64
+	err := d.db.QueryRowxContext(ctx, query, args...).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (d *DB) ExecCtx(ctx context.Context, query string, args ...interface{}) error {
+	_, err := d.db.ExecContext(ctx, query, args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *DB) GetById(dest any, query string, args ...interface{}) error {
+	err := d.db.Get(dest, query, args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
