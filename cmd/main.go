3d0b7e91c084d2df18d5492d36fc641b1027e5d4
Ruslan Kolegov <ruslan@fins.dev>
2023-12-11 20:30:52 +0600

    update main

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,42 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Marsredskies/todo-list/internal/api"
+	"github.com/Marsredskies/todo-list/internal/db"
+	"github.com/Marsredskies/todo-list/internal/envconfig"
+)
+
+var GitCommitSHA string
+
+func main() {
+	log.Printf("Starting with CommitSHA: %s", GitCommitSHA)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	cnf := envconfig.MustGetConfig()
+
+	db.MustApplyMigrations(ctx, cnf)
+
+	api := api.MustInitNewAPI(ctx, cnf)
+
+	api.StartServer(cnf.Port)
+
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, syscall.SIGTERM, syscall.SIGINT)
+
+	defer func() {
+		if err := api.Shutdown(ctx); err != nil {
+			log.Println("error during shutting down the main server: ", err)
+		}
+	}()
+
+	<-exit
+	cancel()
+	log.Println("shutting down")
+}
